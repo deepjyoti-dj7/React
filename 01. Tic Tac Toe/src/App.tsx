577935bc7a5058ad2de2a725ea18422194bd183f
Deepjyoti <deepjyoti0072@gmail.com>
2025-03-12 23:19:02 +0530

    first commit

---
+++ 01. Tic Tac Toe/src/App.tsx
@@ -0,0 +1,12 @@
+import Block from "./components/Block";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="board">
+      <Block />
+    </div>
+  );
+}
+
+export default App;
